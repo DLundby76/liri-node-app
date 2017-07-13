33f95d915dc986e84a376d54d60bba079ff8d3da
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-07-12 21:46:32 -0600

    new file structure

--- keys.js
+++ keys.js
@@ -18,7 +18,7 @@ var spotify = new Spotify({
 });
 
 var request = require('request');
-request('http://www.omdbapi.com/?apikey=[40e9cece]&', function (error, response, body) {
+request('http://www.omdbapi.com/?apikey=40e9cece&', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
